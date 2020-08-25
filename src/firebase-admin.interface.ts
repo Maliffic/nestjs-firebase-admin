5a00e77a6824f96db439987ee0c7e4e8a7d13e1c
Nicholas <nick@handycats.com>
2020-08-25 04:15:06 +0200

    bump deps

--- src/firebase-admin.interface.ts
+++ src/firebase-admin.interface.ts
@@ -1,13 +1,13 @@
-import * as admin from 'firebase-admin';
 import { ModuleMetadata } from '@nestjs/common/interfaces';
+import * as admin from 'firebase-admin';
 
 export type FirebaseUser = admin.auth.DecodedIdToken;
 
 export interface FirebaseAdminModuleAsyncOptions
-  extends Pick<ModuleMetadata, 'imports'> {
-  name?: string;
-  useFactory?: (
-    ...args: any[]
-  ) => Promise<admin.AppOptions> | admin.AppOptions;
-  inject?: any[];
+    extends Pick<ModuleMetadata, 'imports'> {
+    name?: string;
+    useFactory?: (
+        ...args: any[]
+    ) => Promise<admin.AppOptions> | admin.AppOptions;
+    inject?: any[];
 }
--- src/firebase-admin.module.ts
+++ src/firebase-admin.module.ts
@@ -1,53 +1,76 @@
-import { Global, Module, DynamicModule } from '@nestjs/common';
-import { FirebaseAdminModuleAsyncOptions } from './firebase-admin.interface';
-import { FIREBASE_ADMIN_MODULE_OPTIONS, FIREBASE_ADMIN_INJECT } from './firebase-admin.constant';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+import {
+    FIREBASE_ADMIN_INJECT,
+    FIREBASE_ADMIN_MODULE_OPTIONS,
+} from './firebase-admin.constant';
+import { FirebaseAdminModuleAsyncOptions } from './firebase-admin.interface';
 
 @Global()
 @Module({})
 export class FirebaseAdminCoreModule {
-  static forRoot(options: admin.AppOptions): DynamicModule {
-    const firebaseAdminModuleOptions = {
-      provide: FIREBASE_ADMIN_MODULE_OPTIONS,
-      useValue: options,
-    };
+    static forRoot(options: admin.AppOptions): DynamicModule {
+        const firebaseAdminModuleOptions = {
+            provide: FIREBASE_ADMIN_MODULE_OPTIONS,
+            useValue: options,
+        };
 
-    const app = admin.apps.length === 0 ? admin.initializeApp(options) : admin.apps[0];
+        const app =
+            admin.apps.length === 0
+                ? admin.initializeApp(options)
+                : admin.apps[0];
 
-    const firebaseAuthencationProvider = {
-      provide: FIREBASE_ADMIN_INJECT,
-      useValue: app,
-    };
+        const firebaseAuthenticationProvider = {
+            provide: FIREBASE_ADMIN_INJECT,
+            useValue: app,
+        };
 
-    return {
-      module: FirebaseAdminCoreModule,
-      providers: [firebaseAdminModuleOptions, firebaseAuthencationProvider],
-      exports: [firebaseAdminModuleOptions, firebaseAuthencationProvider],
-    };
-  }
+        return {
+            module: FirebaseAdminCoreModule,
+            providers: [
+                firebaseAdminModuleOptions,
+                firebaseAuthenticationProvider,
+            ],
+            exports: [
+                firebaseAdminModuleOptions,
+                firebaseAuthenticationProvider,
+            ],
+        };
+    }
 
-  static forRootAsync(options: FirebaseAdminModuleAsyncOptions): DynamicModule {
-    const firebaseAdminModuleOptions = {
-      provide: FIREBASE_ADMIN_MODULE_OPTIONS,
-      useFactory: options.useFactory,
-      inject: options.inject || [],
-    };
+    static forRootAsync(
+        options: FirebaseAdminModuleAsyncOptions,
+    ): DynamicModule {
+        const firebaseAdminModuleOptions = {
+            provide: FIREBASE_ADMIN_MODULE_OPTIONS,
+            useFactory: options.useFactory,
+            inject: options.inject || [],
+        };
 
-    const firebaseAuthencationProvider = {
-      provide: FIREBASE_ADMIN_INJECT,
-      useFactory: (opt: admin.AppOptions) => {
-        const app = admin.apps.length === 0 ? admin.initializeApp(opt) : admin.apps[0];
+        const firebaseAuthenticationProvider = {
+            provide: FIREBASE_ADMIN_INJECT,
+            useFactory: (opt: admin.AppOptions) => {
+                const app =
+                    admin.apps.length === 0
+                        ? admin.initializeApp(opt)
+                        : admin.apps[0];
 
-        return app;
-      },
-      inject: [FIREBASE_ADMIN_MODULE_OPTIONS],
-    };
+                return app;
+            },
+            inject: [FIREBASE_ADMIN_MODULE_OPTIONS],
+        };
 
-    return {
-      module: FirebaseAdminCoreModule,
-      imports: options.imports,
-      providers: [firebaseAdminModuleOptions, firebaseAuthencationProvider],
-      exports: [firebaseAdminModuleOptions, firebaseAuthencationProvider],
-    };
-  }
+        return {
+            module: FirebaseAdminCoreModule,
+            imports: options.imports,
+            providers: [
+                firebaseAdminModuleOptions,
+                firebaseAuthenticationProvider,
+            ],
+            exports: [
+                firebaseAdminModuleOptions,
+                firebaseAuthenticationProvider,
+            ],
+        };
+    }
 }
